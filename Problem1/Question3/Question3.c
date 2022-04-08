663673381729cc0dd2d2a760f2817881b00836b3
Ravi-Raghavan <56519163+Ravi-Raghavan@users.noreply.github.com>
2022-04-08 14:23:28 -0400

    Update Question3.c

--- Problem1/Question3/Question3.c
+++ Problem1/Question3/Question3.c
@@ -549,6 +549,8 @@ void populateArray(){
 }
 
 void generateTestFile(){
+    srand ( time(NULL) );
+    int MAX = (RAND_MAX) / L;
     FILE *fptr;
     int array[L + H];
     int hiddenLocations[H];
@@ -571,7 +573,7 @@ void generateTestFile(){
             }
         }
         if(set == 1){continue;}
-        array[i] = rand() % 10000000;
+        array[i] = rand() % MAX;
     }
     fptr = fopen("numbers.txt", "w");
     if(fptr == NULL){
